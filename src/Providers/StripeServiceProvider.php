9750280f1be4b3b7494b8f55d8d5b3fc772ea5da
Dmitri Konovalenko <dima92konovalenko@gmail.com>
2023-11-12 16:31:18 +0100

    update: config

--- src/Providers/StripeServiceProvider.php
+++ src/Providers/StripeServiceProvider.php
@@ -35,7 +35,7 @@ class StripeServiceProvider extends ServiceProvider
         $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
 
         $this->publishes([
-            __DIR__.'/../config/stripe.php' => config_path('stripe-plutuss.php'),
+            __DIR__.'/../config/stripe-plutuss.php' => config_path('stripe-plutuss.php'),
         ]);
     }
 }
